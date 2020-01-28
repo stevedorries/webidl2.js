c72b69b19ed7fc9519edbb3a5b78095e760769b7
Steve Dorries <stevedorries@Steves-MacBook-Pro.local>
2020-01-28 15:56:26 -0500

    Fix exports

--- mod.ts
+++ mod.ts
@@ -6,17 +6,17 @@ export {
   parse
 } from "./lib/webidl2.ts";
 
-import { Enum } from "./lib/productions/enum.ts";
-import { Includes } from "./lib/productions/includes.ts";
-import { ExtendedAttributes } from "./lib/productions/extended-attributes.ts";
-import { Typedef } from "./lib/productions/typedef.ts";
-import { CallbackFunction } from "./lib/productions/callback.ts";
-import { Interface } from "./lib/productions/interface.ts";
-import { Mixin } from "./lib/productions/mixin.ts";
-import { Dictionary } from "./lib/productions/dictionary.ts";
-import { Namespace } from "./lib/productions/namespace.ts";
-import { CallbackInterface } from "./lib/productions/callback-interface.ts";
+export { Enum } from "./lib/productions/enum.ts";
+export { Includes } from "./lib/productions/includes.ts";
+export { ExtendedAttributes } from "./lib/productions/extended-attributes.ts";
+export { Typedef } from "./lib/productions/typedef.ts";
+export { CallbackFunction } from "./lib/productions/callback.ts";
+export { Interface } from "./lib/productions/interface.ts";
+export { Mixin } from "./lib/productions/mixin.ts";
+export { Dictionary } from "./lib/productions/dictionary.ts";
+export { Namespace } from "./lib/productions/namespace.ts";
+export { CallbackInterface } from "./lib/productions/callback-interface.ts";
 
 export { write } from "./lib/writer.ts";
 export { validate } from "./lib/validator.ts";
-import { Tokeniser, TokeniserToken, WebIDLParseError } from "./lib/tokeniser.ts";
+export { Tokeniser, TokeniserToken, WebIDLParseError } from "./lib/tokeniser.ts";
