61e1c5f57cf47e80837ef98e820aaf5e1dad8a1c
Steve Dorries <stevedorries@Steves-MacBook-Pro.local>
2020-01-28 15:50:30 -0500

    Update exports

--- mod.ts
+++ mod.ts
@@ -1,4 +1,22 @@
-export { parse } from "./lib/webidl2.ts";
+export {
+  Definition,
+  Partials,
+  Interfaces,
+  Callback,
+  parse
+} from "./lib/webidl2.ts";
+
+import { Enum } from "./lib/productions/enum.ts";
+import { Includes } from "./lib/productions/includes.ts";
+import { ExtendedAttributes } from "./lib/productions/extended-attributes.ts";
+import { Typedef } from "./lib/productions/typedef.ts";
+import { CallbackFunction } from "./lib/productions/callback.ts";
+import { Interface } from "./lib/productions/interface.ts";
+import { Mixin } from "./lib/productions/mixin.ts";
+import { Dictionary } from "./lib/productions/dictionary.ts";
+import { Namespace } from "./lib/productions/namespace.ts";
+import { CallbackInterface } from "./lib/productions/callback-interface.ts";
+
 export { write } from "./lib/writer.ts";
 export { validate } from "./lib/validator.ts";
-export { WebIDLParseError } from "./lib/tokeniser.ts";
+import { Tokeniser, TokeniserToken, WebIDLParseError } from "./lib/tokeniser.ts";
